f12c5443541a070ee0a5db92cbfb54bb145cc8da
RayRay <walrusray811@gmail.com>
2024-07-10 15:52:07 +0900

    init repo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	envErr := godotenv.Load()
+	if envErr != nil {
+		panic(envErr)
+	}
+
+	connString := os.Getenv("RABBITMQ_CONNECTION_STRING")
+
+	conn, connErr := amqp.Dial(connString)
+
+	if connErr != nil {
+		panic(connErr)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ", connString)
+
+	channel, channelErr := conn.Channel()
+
+	if channelErr != nil {
+		panic(channelErr)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/bootdotdev/learn-pub-sub-starter
 
 go 1.22.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+)
